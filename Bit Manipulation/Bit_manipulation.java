1782dc8f202d2520b9e51d1bd80c99542e7b245b
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2023-12-20 22:37:42 +0530

    Create Bit_manipulation.java

---
+++ Bit Manipulation/Bit_manipulation.java
@@ -0,0 +1,9 @@
+public class Bit_Manipulation {
+  
+    public static void main(String args[]){
+       System.out.println((5&6));  // and operator
+       System.out.println((5|6));  // or operator
+       System.out.println((5 ^ 6)); // xor operator
+       System.out.println((~5));  //1's complement (Not operator)
+    }
+}
