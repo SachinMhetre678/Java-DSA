2efb55f3a8971d86311e82a06e04aaf6ad7accd3
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2023-12-23 19:55:15 +0530

    Create Remove_Duplicate.java
    It removes the duplicate letter in string

---
+++ Recursion Basics/Remove_Duplicate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class RecursionBasics {
+
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        //kaam
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a'] == true) {
+            //duplicate
+            removeDuplicates(str, idx+1, newStr, map);
+        } else {
+            map[currChar-'a'] = true;
+            removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[] = {8,3,6,9,5,10,2,5,3};
+        int n = 26;
+        String str = "Ssaacchinmmhetreee";
+        removeDuplicates(str,0,new StringBuilder(" "),new boolean[26]);
+    }
+}
