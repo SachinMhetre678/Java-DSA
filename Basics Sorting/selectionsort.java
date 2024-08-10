f11f46ae9fdf750de8189b080b08f2c8226c734f
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-08-10 10:32:28 +0530

    Update selectionsort.java

--- Basics Sorting/selectionsort.java
+++ Basics Sorting/selectionsort.java
@@ -1,29 +1,39 @@
-import java.util.*;
+package Sorting;
 
-    public static void selectionsort(int a[]){
-        for(int i=0;i< a.length-1;i++){
-            int minPos = i;
-            for(int j= i+1;j< a.length;j++){
-                if(a[minPos]>a[j]){
-                    minPos = j;
-                }
+public class SelectionSort {
+
+    //this function return index of min element in arr in the range start to end-1
+    // [1,2,3,9,6,8,5,4,9] , start = 3 , minindex = 3, i =4 ,
+    public static int getMinIndex(int[] arr,int start){
+        int minIndex = start; //intially we assume that start element as min index
+        for(int i = start + 1 ; i<arr.length;i++){
+            // we go in the remaining array from [start+1,end-1]
+            if(arr[i] < arr[minIndex]){
+                //compare if min element is better than the last found element
+                minIndex = i;
             }
-            //Swap
-            int temp = a[minPos];
-            a[minPos] = a[i];
-            a[i] = temp;
         }
+        return minIndex;
     }
 
-    public static void printArr(int a[]){
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i] + " ");
+
+    //this function applies selection sort on the given array and arranges element in inx order
+    public static void selectionSort(int[] arr){
+        for(int i = 0; i< arr.length;i++){
+            int minIndex = getMinIndex(arr,i); // get min element from arr
+            if(i != minIndex){ //if the ith elemet is the min dont swap
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
         }
     }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,9,6,8,5,4,9};
+        selectionSort(arr);
+        for(int i = 0 ; i < arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
 
-    public static void main(String args[]){
-        int a[] = {8,99,1,6,9,18,3,16};
-        selectionsort(a);
-        printArr(a);
     }
 }
